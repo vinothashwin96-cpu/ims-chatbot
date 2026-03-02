b7c5b7e467691326697d1fb107b95bc0a13b9b24
vinothashwin96@outlook.com <vinothashwin96@outlook.com>
2026-03-02 22:16:46 +0530

    Initial backend setup

---
+++ backend/ims-chatbot/src/test/java/com/example/ims_chatbot/ImsChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ims_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
